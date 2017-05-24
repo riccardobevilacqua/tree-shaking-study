04471f083875b6207a2bcdba5070695ec3bc3a77
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2017-05-24 22:04:27 +0200

    Updated file reader

--- src/js/readFile.ts
+++ src/js/readFile.ts
@@ -2,8 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 
+let inputFilePath = './examples/fn/01/index.js';
 let data = '';
-let readableStream = fs.createReadStream(path.resolve('./examples/fn/01/index.js'));
+let readableStream = fs.createReadStream(path.resolve(inputFilePath));
 
 readableStream.on('data', function(chunk) {
     data+=chunk;
