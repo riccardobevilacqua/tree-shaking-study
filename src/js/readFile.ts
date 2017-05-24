ac88bde0bf1332201e775f903b9a5140c4fc14e0
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2017-05-24 23:50:49 +0200

    Enhanced file reader

--- src/js/readFile.ts
+++ src/js/readFile.ts
@@ -1,15 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const readline = require('readline');
 
 let inputFilePath = './examples/fn/01/index.js';
-let data = '';
 let readableStream = fs.createReadStream(path.resolve(inputFilePath));
 
-readableStream.on('data', function(chunk) {
-    data+=chunk;
-});
-
-readableStream.on('end', function() {
-    console.log(data);
-});
+readableStream.pipe(process.stdout);
