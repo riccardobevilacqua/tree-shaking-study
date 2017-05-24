94cc701666da6a229da60e6fa66d0ada8de5aea7
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2017-05-25 00:15:57 +0200

    Added file writing

--- src/js/readFile.ts
+++ src/js/readFile.ts
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 let inputFilePath = './examples/fn/01/index.js';
+let outputFilePath = './dist/output-01.js';
 let readableStream = fs.createReadStream(path.resolve(inputFilePath));
+let writebleStream = fs.createWriteStream(path.resolve(outputFilePath));
 
-readableStream.pipe(process.stdout);
+readableStream.pipe(writebleStream);
